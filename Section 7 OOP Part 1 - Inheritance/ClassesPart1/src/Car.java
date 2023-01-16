43eabd3dad466c35a33c2fd2b256287c31287f82
charanreddy404 <charanlucky6143@gmail.com>
2023-01-16 23:47:17 +0530

    class example

--- Section 7 OOP Part 1 - Inheritance/ClassesPart1/src/Car.java
+++ Section 7 OOP Part 1 - Inheritance/ClassesPart1/src/Car.java
@@ -1,3 +1,11 @@
 public class Car {
+    private String make="Tesla";
+    private String model="Model X";
+    private String color="Gray";
+    private int doors=2;
+    private boolean convertible = true;
 
+    public void describCar(){
+        System.out.println(doors+"-Door "+color+" "+make+" "+model+" "+(convertible?"Convertible" : ""));
+    }
 }
--- Section 7 OOP Part 1 - Inheritance/ClassesPart1/src/Main.java
+++ Section 7 OOP Part 1 - Inheritance/ClassesPart1/src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.describCar();
     }
 }
