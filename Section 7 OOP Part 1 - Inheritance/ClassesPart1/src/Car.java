8e50cba1f98a353c4dd7fa53fdc876867d4d4e9f
charanreddy404 <charanlucky6143@gmail.com>
2023-01-16 23:57:24 +0530

    getters in class

--- Section 7 OOP Part 1 - Inheritance/ClassesPart1/src/Car.java
+++ Section 7 OOP Part 1 - Inheritance/ClassesPart1/src/Car.java
@@ -1,9 +1,29 @@
 public class Car {
-    public String make="Tesla";
-    public String model="Model X";
-    public String color="Gray";
+    private String make="Tesla";
+    private String model="Model X";
+    private String color="Gray";
     private int doors=2;
     private boolean convertible = true;
+
+    public int getMake() {
+        return doors;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
 
     public void describCar(){
         System.out.println(doors+"-Door "+color+" "+make+" "+model+" "+(convertible?"Convertible" : ""));
