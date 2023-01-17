021122e9ebb94c0658bee1be39ba681b905ebc22
charanreddy404 <charanlucky6143@gmail.com>
2023-01-17 07:17:42 +0530

    class Exercise completed

--- Section 7 OOP Part 1 - Inheritance/ClassesChallenge/src/Main.java
+++ Section 7 OOP Part 1 - Inheritance/ClassesChallenge/src/Main.java
@@ -1,13 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        BankAccount accountOne = new BankAccount();
-        accountOne.setCustomerName("Charan");
-        accountOne.setAccountNumber("111");
-        accountOne.setEmail("chara@java.in");
-        accountOne.setPhoneNumber("999999999");
-        accountOne.setAccountBalance(1000.00);
+//        BankAccount accountOne = new BankAccount();
+//        accountOne.setCustomerName("Charan");
+//        accountOne.setAccountNumber("111");
+//        accountOne.setEmail("chara@java.in");
+//        accountOne.setPhoneNumber("999999999");
+//        accountOne.setAccountBalance(1000.00);
+//
+//        accountOne.depositAmount(200.00);
+//        accountOne.withdrawAmount(1200.00);
 
-        accountOne.depositAmount(200.00);
-        accountOne.withdrawAmount(1200.00);
+//
+
+        Person person = new Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
     }
 }
