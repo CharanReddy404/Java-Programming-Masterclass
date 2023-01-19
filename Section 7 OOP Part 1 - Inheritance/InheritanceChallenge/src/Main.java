f6a9cc1cdd121b851c6d3a2ba418d3cc83454d3b
charanreddy404 <charanlucky6143@gmail.com>
2023-01-19 09:08:40 +0530

    Cylinder challenge completed

--- Section 7 OOP Part 1 - Inheritance/InheritanceChallenge/src/Main.java
+++ Section 7 OOP Part 1 - Inheritance/InheritanceChallenge/src/Main.java
@@ -1,19 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        Employee tim = new Employee("Tim","11/11/1985","01/01/2022");
-        System.out.println(tim);
-        System.out.println("Age = "+ tim.getAge());
-        System.out.println("Pay = "+ tim.collectPay());
 
-        SalariedEmployee joe = new SalariedEmployee("Joe","11/11/1990","03/03/2020",35000);
-        System.out.println(joe);
-        System.out.println("Joe's Paycheck = $"+joe.collectPay());
-        joe.retire();
-        System.out.println("Joe's Pension check = $"+joe.collectPay());
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius= " + circle.getRadius());
+        System.out.println("circle.area= " + circle.getArea());
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.radius= " + cylinder.getRadius());
+        System.out.println("cylinder.height= " + cylinder.getHeight());
+        System.out.println("cylinder.area= " + cylinder.getArea());
+        System.out.println("cylinder.volume= " + cylinder.getVolume());
 
-        HourlyEmployee mary = new HourlyEmployee("Mary","05/05/ 1970", "03/03/2021", 15);
-        System.out.println(mary);
-        System.out.println("Mary's Paycheck = $"+mary.collectPay());
-        System.out.println("Mary's Holiday Pay = $"+mary.getDoublePay());
+//          Employer------------------------------------------------------>
+//        Employee tim = new Employee("Tim","11/11/1985","01/01/2022");
+//        System.out.println(tim);
+//        System.out.println("Age = "+ tim.getAge());
+//        System.out.println("Pay = "+ tim.collectPay());
+//
+//        SalariedEmployee joe = new SalariedEmployee("Joe","11/11/1990","03/03/2020",35000);
+//        System.out.println(joe);
+//        System.out.println("Joe's Paycheck = $"+joe.collectPay());
+//        joe.retire();
+//        System.out.println("Joe's Pension check = $"+joe.collectPay());
+//
+//        HourlyEmployee mary = new HourlyEmployee("Mary","05/05/ 1970", "03/03/2021", 15);
+//        System.out.println(mary);
+//        System.out.println("Mary's Paycheck = $"+mary.collectPay());
+//        System.out.println("Mary's Holiday Pay = $"+mary.getDoublePay());
     }
 }
