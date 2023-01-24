481d082e4d1e932e65b6eec33a220073494156af
charanreddy404 <charanlucky6143@gmail.com>
2023-01-24 06:06:26 +0530

    interfaces

---
+++ Section 11 Inner and Abstract Classes & Interfaces/Interfaces/src/come/dev/Gearbox.java
@@ -0,0 +1,9 @@
+package come.dev;
+
+public class Gearbox {
+    private boolean clutchIsIn;
+
+    public void operateClutch(boolean inOrOut){
+        this.clutchIsIn = inOrOut;
+    }
+}
---
+++ Section 11 Inner and Abstract Classes & Interfaces/Interfaces/src/come/dev/Main.java
@@ -0,0 +1,10 @@
+package come.dev;
+
+public class Main {
+    public static void main(String[] args) {
+        ITelephone timsPhone = new DeskPhone(123456);
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();
+    }
+}
