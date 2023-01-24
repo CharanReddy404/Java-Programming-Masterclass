83807d223e93fb2c1aa38446096e51c411eb5d22
charanreddy404 <charanlucky6143@gmail.com>
2023-01-24 10:14:50 +0530

    interface challenge completed

--- Section 11 Inner and Abstract Classes & Interfaces/InterfaceChallenge/src/com/charan/ISaveable.java
+++ Section 11 Inner and Abstract Classes & Interfaces/InterfaceChallenge/src/com/charan/ISaveable.java
@@ -1,9 +1,8 @@
 package com.charan;
-
 import java.util.List;
 
 public interface ISaveable {
+    // write code here
     List<String> write();
-    void read(List<String> savedValues);
-
+    void read(List<String> values);
 }
