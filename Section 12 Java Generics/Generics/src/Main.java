a40a2f642fcc4471ad912189bd7546a7cb4fefe7
charanreddy404 <charanlucky6143@gmail.com>
2023-01-25 11:36:29 +0530

    Generics

--- Section 12 Java Generics/Generics/src/Main.java
+++ Section 12 Java Generics/Generics/src/Main.java
@@ -1,23 +1,41 @@
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> items = new ArrayList<Integer>();
-        items.add(1);
-        items.add(2);
-        items.add(3);
-//        items.add("charan");
-        items.add(4);
-        items.add(5);
 
-        printDouble(items);
+        Team<FootballPlayer> silverEagle = new Team<>("Silver Eagle");
+        FootballPlayer charan = new FootballPlayer("charan");
+        silverEagle.addPlayer(charan);
+
+        Team<BaseballPlayer> blackBulls = new Team<>("Black Bulls");
+        BaseballPlayer lucky = new BaseballPlayer("lucky");
+        blackBulls.addPlayer(lucky);
+
+        Team<SoccerPlayer> goldenDons = new Team<>("Golden Dons");
+        SoccerPlayer Reddy = new SoccerPlayer("Reddy");
+        goldenDons.addPlayer(Reddy);
+
+        Team<FootballPlayer> silverEagleOne = new Team<>("silverEagleOne");
+        Team<FootballPlayer> silverEagleTwo = new Team<>("silverEagleTwo");
+
+        silverEagleOne.matchResult(silverEagle,2,1);
+        silverEagleTwo.matchResult(silverEagleTwo,1,1);
+
+
+//        ArrayList<Integer> items = new ArrayList<Integer>();
+//        items.add(1);
+//        items.add(2);
+//        items.add(3);
+////        items.add("charan");
+//        items.add(4);
+//        items.add(5);
+//
+//        printDouble(items);
     }
 
-    private static void printDouble(ArrayList<Integer> n){
-        for (int i:n){
-            System.out.println((Integer)i*2);
-        }
-    }
+//    private static void printDouble(ArrayList<Integer> n){
+//        for (int i:n){
+//            System.out.println((Integer)i*2);
+//        }
+//    }
 
 }
